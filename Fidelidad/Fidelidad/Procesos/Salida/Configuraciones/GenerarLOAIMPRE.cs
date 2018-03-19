a53f886e97b5b0e514a46ea4657817205c0fc2cb
Gonzalo <glucero@hexacta.com>
2018-03-19 11:41:50 -0300

    Configuracion LOAIMPRE

---
+++ Fidelidad/Fidelidad/Procesos/Salida/Configuraciones/GenerarLOAIMPRE.cs
@@ -0,0 +1,141 @@
+﻿using Hexacta.YPF.Fidelizacion.Core.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hexacta.YPF.Fidelizacion.Core.Procesos
+{
+    public static class GenerarLOAIMPRE
+    {
+
+        public static Archivo Generar()
+        {
+            Archivo archivo = new Archivo()
+            {
+                Nombre = "LOAIMPRE",
+                OrigenDatos = "mockLOAIMPRE",
+                IsUnixSaltoLinea = true
+            };
+            archivo.Cabecera = GenerarCabecera();
+            archivo.Detalle = GenerarRegistro();
+
+            return archivo;
+        }
+
+        private static Cabecera GenerarCabecera()
+        {
+            Cabecera cabecera = new Cabecera();
+            cabecera.NombreTabla = "cabecera";
+            cabecera.Campos = new List<CampoCabecera>();
+
+            CampoCabecera campoCabecera = new CampoCabecera()
+            {
+                NombreCampo = "COD-EESS",
+                NombreBaseDeDatos = "CodigoEstacion",
+                Descripcion = "Código de Estación",
+                Longitud = 5,
+                Offset = 0,
+                PadCaracter = '0',
+                IsPadLeft = true
+            };
+            cabecera.Campos.Add(campoCabecera);
+
+            campoCabecera = new CampoCabecera()
+            {
+                NombreCampo = "FECHA",
+                NombreBaseDeDatos = "Fecha",
+                Descripcion = "Fecha de creacion archivo AAAAMMDD",
+                Longitud = 8,
+                Offset = 5,
+                PadCaracter = '0',
+                IsPadLeft = true
+            };
+            cabecera.Campos.Add(campoCabecera);
+
+            campoCabecera = new CampoCabecera()
+            {
+                NombreCampo = "HORA",
+                NombreBaseDeDatos = "hora",
+                Descripcion = "Hora de creacion archivo HHMM",
+                Longitud = 4,
+                Offset = 13,
+                PadCaracter = '0',
+                IsPadLeft = true
+            };
+            cabecera.Campos.Add(campoCabecera);
+
+            campoCabecera = new CampoCabecera()
+            {
+                NombreCampo = "FRECAMBIO",
+                NombreBaseDeDatos = "FlagRecambio",
+                Descripcion = "Flag de Actualización de Lista N = Novedades",
+                Longitud = 1,
+                Offset = 17,
+                PadCaracter = '0',
+                IsPadLeft = true
+            };
+            cabecera.Campos.Add(campoCabecera);
+
+            campoCabecera = new CampoCabecera()
+            {
+                NombreCampo = "VERSIONACES",
+                NombreBaseDeDatos = "version",
+                Descripcion = "Versión de los datos de Serviclub",
+                Longitud = 5,
+                Offset = 18,
+                PadCaracter = '0',
+                IsPadLeft = true
+            };
+            cabecera.Campos.Add(campoCabecera);
+
+            return cabecera;
+        }
+
+        private static Detalle GenerarRegistro()
+        {
+            Detalle detalle = new Detalle();
+            detalle.NombreTabla = "registro";
+            detalle.Campos = new List<CampoDetalle>();
+            
+            CampoDetalle campoDetalle = new CampoDetalle()
+            {
+                NombreCampo = "TIPO-TARJ",
+                NombreBaseDeDatos = "TipoTarjeta",
+                Descripcion = "Tipo de tarjeta",
+                Longitud = 2,
+                Offset = 0,
+                PadCaracter = '0',
+                IsPadLeft = true
+            };
+            detalle.Campos.Add(campoDetalle);
+
+            campoDetalle = new CampoDetalle()
+            {
+                NombreCampo = "ID-SECCION",
+                NombreBaseDeDatos = "IdSeccion",
+                Descripcion = "Id de la seccion",
+                Longitud = 3,
+                Offset = 2,
+                PadCaracter = '0',
+                IsPadLeft = true
+            };
+            detalle.Campos.Add(campoDetalle);
+
+            campoDetalle = new CampoDetalle()
+            {
+                NombreCampo = "TEXTO",
+                NombreBaseDeDatos = "Texto",
+                Descripcion = "Texto que se imprimirá en el ticket.",
+                Longitud = 300,
+                Offset = 5,
+                PadCaracter = '0',
+                IsPadLeft = true
+            };
+            detalle.Campos.Add(campoDetalle);
+
+            return detalle;
+        }
+    }
+}
